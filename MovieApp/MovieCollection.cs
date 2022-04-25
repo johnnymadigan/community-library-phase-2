0e19d3659a49746622a2da152e8f2224a7c1106b
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-04-25 21:48:06 +1000

    Implemented BST methods

--- MovieApp/MovieCollection.cs
+++ MovieApp/MovieCollection.cs
@@ -1,4 +1,4 @@
-// Phase 2
+﻿// Phase 2
 // An implementation of MovieCollection ADT
 // 2022
 
@@ -64,7 +64,7 @@ public class MovieCollection : IMovieCollection
 	// Post-condition: return true if this movie collection is empty; otherwise, return false.
 	public bool IsEmpty()
 	{
-		//To be completed
+		return root == null;
 	}
 
 	// Insert a movie into this movie collection
@@ -72,9 +72,34 @@ public class MovieCollection : IMovieCollection
 	// Post-condition: the movie has been added into this movie collection
 	public bool Insert(IMovie movie)
 	{
-		//To be completed
+		if (!Search(movie)) // invariant: no duplicates in this movie collection
+		{
+			if (root == null) root = new BTreeNode(movie); // empty tree? first node
+			else Insert(movie, root); // kick off recursive travel to insert as leaf
+			return true;
+		}
+		else return false;
 	}
 
+	// PUBLIC INSERT ensures the first call of this method is with a root that isn't null
+	//
+	// pre: ptr != null
+	// post: item is inserted to the binary search tree rooted at ptr
+	// recursive
+	private void Insert(IMovie movie, BTreeNode parent)
+	{
+		// 
+		if (movie.CompareTo(parent.Movie) < 0)
+		{
+			if (parent.LChild == null) parent.LChild = new BTreeNode(movie);
+			else Insert(movie, parent.LChild);
+		}
+		else
+		{
+			if (parent.RChild == null) parent.RChild = new BTreeNode(movie);
+			else Insert(movie, parent.RChild);
+		}
+	}
 
 
 	// Delete a movie from this movie collection
@@ -83,6 +108,84 @@ public class MovieCollection : IMovieCollection
 	public bool Delete(IMovie movie)
 	{
 		//To be completed
+		// there are three cases to consider:
+		// 1. the node to be deleted is a leaf
+		// 2. the node to be deleted has only one child 
+		// 3. the node to be deleted has both left and right children
+
+
+
+		// search for item and its parent
+		BTreeNode curr = root; // current search reference
+		BTreeNode parent = null; // parent of current
+
+		// while current aint null AND the queried movie does not match the current's movie (found)
+		// we need this kind of search here to keep track of the parent, but all g as BST super quick
+		while ((curr != null) && (movie.CompareTo(curr.Movie) != 0))
+		{
+			// Traverse to a child node (updating current) and remember the parent
+			parent = curr;
+			if (movie.CompareTo(curr.Movie) < 0) curr = curr.LChild;	// Movie in left subtree
+			else curr = curr.RChild;									// Movie in right subtree
+		}
+
+		if (curr != null) // if the search was successful aka current is now the node to DELETE
+		{
+			// case 3: ITEM HAS 2 CHILDREN
+			if ((curr.LChild != null) && (curr.RChild != null))
+			{
+				// find the right-most node in left subtree of current
+				if (curr.LChild.RChild == null) // a special case: the right subtree of ptr.LChild is empty
+				{
+					// replace current's movie with it's left node's movie (keeps correct order)
+					// set pointer to point to left child's left child (right is null remember) effectively dropping,
+					// C#'s garbage collection is better than C
+					curr.Movie = curr.LChild.Movie;
+					curr.LChild = curr.LChild.LChild;
+				}
+				else // if the left child's subtree has both left and right children again
+				{
+					// quickly store left child subtree parent and left child
+					BTreeNode p = curr.LChild;
+					BTreeNode pp = curr; // parent of p (kinda uselesss making it current but good practice for default value)
+
+					// travel to most right leaf node starting from tHAT subtree and keep track of the parent
+					while (p.RChild != null)
+					{
+						pp = p;
+						p = p.RChild;
+					}
+					// replace current's movie with this far right (keeps correct order)
+					// set pointer to point to left child's left child (right is null remember) effectively dropping,
+					// C#'s garbage collection is better than C
+					// copy the item at p to ptr
+					curr.Movie = p.Movie;
+					pp.RChild = p.LChild;
+				}
+			}
+			else // cases 1 & 2: item has no or only one child
+			{
+				// temp store the single child
+				BTreeNode c;
+				if (curr.LChild != null)
+					c = curr.LChild;
+				else
+					c = curr.RChild;
+
+				// remove node current
+				if (curr == root) //need to change root
+					root = c;
+				else
+				{
+					if (curr == parent.LChild)
+						parent.LChild = c;
+					else
+						parent.RChild = c;
+				}
+			}
+			return true; // deletion complete
+		}
+		else return false; // not found
 	}
 
 	// Search for a movie in this movie collection
@@ -91,7 +194,7 @@ public class MovieCollection : IMovieCollection
 	//	     otherwise, return false.
 	public bool Search(IMovie movie)
 	{
-		//To be completed
+		return Search(movie.Title, root) != null;
 	}
 
 	// Search for a movie by its title in this movie collection  
@@ -100,10 +203,22 @@ public class MovieCollection : IMovieCollection
 	//	     otherwise, return null.
 	public IMovie Search(string movietitle)
 	{
-		//To be completed
+		return Search(movietitle, root);
 	}
 
-
+	private IMovie Search(string title, BTreeNode parent)
+	{
+		// recursive, return if the object if found, treats each recursion's root as the root
+		// of 0,1,2 children (subtree) r is the subtree's root, NOT THE ENTIRE TREE
+		// but obviously starts with the root of the entire tree as the first one
+		if (parent != null)
+		{
+			if (title.CompareTo(parent.Movie) == 0) return parent.Movie;
+			else if (title.CompareTo(parent.Movie) < 0) return Search(title, parent.LChild);
+			else return Search(title, parent.RChild);
+		}
+		else return null;
+	}
 
 	// Store all the movies in this movie collection in an array in the dictionary order by their titles
 	// Pre-condition: nil
@@ -120,7 +235,7 @@ public class MovieCollection : IMovieCollection
 	// Post-condition: all the movies have been removed from this movie collection 
 	public void Clear()
 	{
-		//To be completed
+		root = null;
 	}
 }
 
