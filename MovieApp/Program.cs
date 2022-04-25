d0d0b7f464b4db7068ea4a1db8f246091f757f12
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-04-25 23:14:19 +1000

    Began testing

--- MovieApp/Program.cs
+++ MovieApp/Program.cs
@@ -7,6 +7,20 @@ namespace MovieApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            IMovie handmaiden = new Movie("handmaiden", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
+            IMovie eeaao = new Movie("eeaao", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
+            IMovie batman = new Movie("batman", MovieGenre.Action, MovieClassification.M15Plus, 999, 1);
+            IMovie arcane = new Movie("arcane", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
+
+            IMovieCollection lib = new MovieCollection();
+
+            lib.Insert(handmaiden);
+            lib.Insert(eeaao);
+            lib.Insert(batman);
+            lib.Insert(arcane);
+
+            foreach (IMovie m in lib.ToArray()) Console.Write((m).Title + "\n");
         }
     }
 }
