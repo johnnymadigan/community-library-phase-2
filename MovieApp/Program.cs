361b319899efa42b52b1cdffeb2b64fa4e27b042
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-04-27 18:00:53 +1000

    Started tests

--- MovieApp/Program.cs
+++ MovieApp/Program.cs
@@ -4,24 +4,63 @@ namespace MovieApp
 {
     class Program
     {
-        static void Main(string[] args)
+		public static IMovieCollection lib = new MovieCollection();
+
+		static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            IMovie handmaiden = new Movie("handmaiden", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
             IMovie eeaao = new Movie("eeaao", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
-            IMovie batman = new Movie("batman", MovieGenre.Action, MovieClassification.M15Plus, 999, 1);
+			IMovie handmaiden = new Movie("handmaiden", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
+			IMovie batman = new Movie("batman", MovieGenre.Action, MovieClassification.M15Plus, 999, 1);
             IMovie arcane = new Movie("arcane", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
+            IMovie ricknmorty = new Movie("ricknmorty", MovieGenre.Comedy, MovieClassification.M15Plus, 999, 1);
+            IMovie extracurricular = new Movie("extracurricular", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
 
-            IMovieCollection lib = new MovieCollection();
-
-            lib.Insert(handmaiden);
+			Console.WriteLine("Adding 6 movies to BST...");
             lib.Insert(eeaao);
-            lib.Insert(batman);
-            lib.Insert(arcane);
+			lib.Insert(handmaiden);
+			lib.Insert(batman);
+			lib.Insert(arcane);
+			lib.Insert(ricknmorty);
+			lib.Insert(extracurricular);
+			Print();
 
-            foreach (IMovie m in lib.ToArray()) Console.Write((m).Title + "\n");
+			Console.WriteLine($"Deleting node (w 2 children) ({handmaiden.Title}) from BST...");
+			lib.Delete(handmaiden);
+			Print();
+
+			Console.WriteLine($"Deleting leaf (no children) ({arcane.Title}) from BST...");
+			lib.Delete(arcane);
+			Print();
+			
+			Console.Write($"Deleting root ({eeaao.Title}) from BST...");
+			lib.Delete(eeaao);
+			Console.Write($"root is now ({lib.Root})\n");
+			Print();
+
+			Console.Write($"Adding old root ({eeaao.Title}) back as node...");
+			lib.Insert(eeaao);
+			Console.Write($"root is still ({lib.Root})\n");
+			Print();
+
+			//// SEARCH TESTS HERE (Obj n Title)
+
+			Console.WriteLine("clearing BST...");
+			lib.Clear();
+			Print();
+		}
+
+		public static void Print()
+        {
+			// demonstrates in-order traversal to get the array
+			Console.WriteLine("\n━━┅━━━┅━━┅━━╾╮");
+			foreach (IMovie m in lib.ToArray()) Console.Write((m).Title + "\n");
+			Console.WriteLine("━━┅━━━┅━━┅━━╾╯");
+		}
+
+		public static bool MovieTests()
+        {
+			return false;
         }
-    }
+	}
 }
 
