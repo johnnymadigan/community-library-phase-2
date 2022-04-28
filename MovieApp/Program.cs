2d45f581d23f0a1fc42787cc85a515cf6776e1db
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-04-28 21:16:21 +1000

    Refactored tests

--- MovieApp/Program.cs
+++ MovieApp/Program.cs
@@ -6,52 +6,28 @@ namespace MovieApp
     {
 		public static IMovieCollection lib = new MovieCollection();
 
+		public static IMovie eeaao = new Movie("eeaao", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
+		public static IMovie handmaiden = new Movie("handmaiden", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
+		public static IMovie batman = new Movie("batman", MovieGenre.Action, MovieClassification.M15Plus, 999, 1);
+		public static IMovie arcane = new Movie("arcane", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
+		public static IMovie ricknmorty = new Movie("ricknmorty", MovieGenre.Comedy, MovieClassification.M15Plus, 999, 1);
+		public static IMovie extracurricular = new Movie("extracurricular", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
+
 		static void Main(string[] args)
         {
-            IMovie eeaao = new Movie("eeaao", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
-			IMovie handmaiden = new Movie("handmaiden", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
-			IMovie batman = new Movie("batman", MovieGenre.Action, MovieClassification.M15Plus, 999, 1);
-            IMovie arcane = new Movie("arcane", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
-            IMovie ricknmorty = new Movie("ricknmorty", MovieGenre.Comedy, MovieClassification.M15Plus, 999, 1);
-            IMovie extracurricular = new Movie("extracurricular", MovieGenre.Drama, MovieClassification.M15Plus, 999, 1);
-
-			Console.WriteLine("Adding 6 movies to BST...");
-            lib.Insert(eeaao);
-			lib.Insert(handmaiden);
-			lib.Insert(batman);
-			lib.Insert(arcane);
-			lib.Insert(ricknmorty);
-			lib.Insert(extracurricular);
-			Print();
-
-			Console.WriteLine($"Deleting node (w 2 children) ({handmaiden.Title}) from BST...");
-			lib.Delete(handmaiden);
-			Print();
-
-			Console.WriteLine($"Deleting leaf (no children) ({arcane.Title}) from BST...");
-			lib.Delete(arcane);
-			Print();
-			
-			Console.Write($"Deleting root ({eeaao.Title}) from BST...");
-			lib.Delete(eeaao);
-			Console.Write($"root is now ({lib.Root})\n");
-			Print();
-
-			Console.Write($"Adding old root ({eeaao.Title}) back as node...");
-			lib.Insert(eeaao);
-			Console.Write($"root is still ({lib.Root})\n");
-			Print();
-
-			//// SEARCH TESTS HERE (Obj n Title)
-
-			Console.WriteLine("clearing BST...");
-			lib.Clear();
-			Print();
+			MovieTests();
+			AddTests();
+			DeleteTests();
+			ClearTests();
 		}
+
+
+
+
 
 		public static void Print()
         {
-			// demonstrates in-order traversal to get the array
+			// IN-ORDER TRAVERSAL TESTS
 			Console.WriteLine("\n━━┅━━━┅━━┅━━╾╮");
 			foreach (IMovie m in lib.ToArray()) Console.Write((m).Title + "\n");
 			Console.WriteLine("━━┅━━━┅━━┅━━╾╯");
@@ -61,6 +37,67 @@ namespace MovieApp
         {
 			return false;
         }
+
+		public static void AddTests()
+        {
+			Console.WriteLine("Adding 6 movies to BST...");
+			lib.Insert(eeaao);
+			lib.Insert(handmaiden);
+			lib.Insert(batman);
+			lib.Insert(arcane);
+			lib.Insert(ricknmorty);
+			lib.Insert(extracurricular);
+			Print();
+		}
+
+		public static void DeleteTests()
+        {
+			Console.WriteLine($"\nDeleting node (w 2 children) ({handmaiden.Title}) from BST...");
+			lib.Delete(handmaiden);
+			Print();
+
+			Console.WriteLine($"\nDeleting leaf (no children) ({arcane.Title}) from BST...");
+			lib.Delete(arcane);
+			Print();
+
+			Console.WriteLine($"\nAlready deleted ({arcane.Title}) no change...");
+			lib.Delete(arcane);
+			Print();
+
+			Console.Write($"\nDeleting root ({eeaao.Title}) from BST...");
+			lib.Delete(eeaao);
+			Console.Write($"root is now ({lib.Root})\n");
+			Print();
+
+			Console.Write($"\nAdding old root ({eeaao.Title}) back as node...");
+			lib.Insert(eeaao);
+			Console.Write($"root is still ({lib.Root})\n");
+			Print();
+		}
+
+		public static void SearchTests()
+        {
+			string s;
+
+			s = lib.Search("eeaao") != null ? $"✔ (found via string)" : $"✘ (!found via string)";
+			Console.WriteLine($"\n✔ = {s}");
+
+			s = lib.Search("arcane") != null ? $"✔ (found via string)" : $"✘ (!found via string)";
+			Console.WriteLine($"✘ = {s}");
+
+			s = lib.Search(eeaao) ? $"✔ (found via obj)" : $"✘ (!found via obj)";
+			Console.WriteLine($"✔ = {s}");
+
+			s = lib.Search(arcane) ? $"✔ (found via obj)" : $"✘ (!found via obj)";
+			Console.WriteLine($"✘ = {s}");
+		}
+
+		public static void ClearTests()
+        {
+			Console.WriteLine("\nclearing BST...");
+			lib.Clear();
+			Print();
+		}
 	}
 }
 
